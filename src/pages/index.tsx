2b76313eedfea8028fe0a4008f7601ff797988a0
maithevien9 <vienthe.mai@team.enouvo.com>
2023-04-18 22:22:45 +0700

    feat(init-project): test pre-commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,18 @@
-import Card from 'shared/components/Card'
-import { useFetchRepositories } from 'shared/hooks/useRepos'
-import useFavoriteReposStore from 'store/favoriteRepos'
+import Card from 'shared/components/Card';
+import { useFetchRepositories } from 'shared/hooks/useRepos';
+import useFavoriteReposStore from 'store/favoriteRepos';
 
 export default function IndexPage() {
-  const { data, isLoading } = useFetchRepositories('maithevien9')
-  const { favoriteReposIds } = useFavoriteReposStore()
+  const { data, isLoading } = useFetchRepositories('maithevien9');
+  const { favoriteReposIds } = useFavoriteReposStore();
 
-  if (isLoading) return <div>Loading...</div>
+  if (isLoading) return <div>Loading....</div>;
 
   return (
     <div>
-      {data?.map(repository => (
+      {data?.map((repository) => (
         <Card isFavorite={favoriteReposIds.includes(repository.id)} key={repository.id} repository={repository} />
       ))}
     </div>
-  )
+  );
 }
